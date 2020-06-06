d7c5c02823703c8ff55054705fbdd435a1d783c1
Ravi Hammond <ravihammond@gmail.com>
2020-06-06 15:10:27 +0930

    Updates

---
+++ precache-manifest.f1f42cfdfd5603509d9c40f1b9cb23e9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed45abfa9d1817c35268a3385001d4cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1a2e457abf35dad129a0",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "857fd94429ca8ded7f8e",
+    "url": "/static/js/2.fcbfac65.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fcbfac65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a2e457abf35dad129a0",
+    "url": "/static/js/main.5ae87f8b.chunk.js"
+  },
+  {
+    "revision": "30a0b2ad91014fc70e9c",
+    "url": "/static/js/runtime-main.896852f5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
